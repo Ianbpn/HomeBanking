5a8aeac0e017e9ec91eb347b5b079db33366cc01
Sol <Sol@DESKTOP-M7F02AS>
2024-05-27 13:21:18 -0300

    Add project files.

---
+++ Database/Models/DBInitializer.cs
@@ -0,0 +1,26 @@
+﻿namespace HomeBanking.Database.Models
+{
+    public class DBInitializer
+    {
+        public static void Initialize(HomeBankingContext context)
+        {
+            if (!context.Clients.Any())
+            {
+                var clients = new Client[]
+                {
+                    new Client {Email= "ianb.782@live.com",FirstName="Ian",
+                        LastName="Pereyra",Password="123456"},
+                    new Client {Email= "marcelo@gmail.com",FirstName="Marcelo",
+                        LastName="Diaz",Password="123456"},
+                    new Client {Email= "julieta@gmail.com",FirstName="Julieta",
+                        LastName="Gonzalez",Password="123456"},
+                    new Client {Email= "nicolas@gmail.com",FirstName="Nicolas",
+                        LastName="Blanco",Password="123456"}
+                };
+                context.Clients.AddRange(clients);
+                //Guardar los cambios en la base de datos
+                context.SaveChanges();
+            }
+        }
+    }
+}
